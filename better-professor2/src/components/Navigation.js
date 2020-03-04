fec242a9d1612e5a852c54cab90249230c348664
ranraok <46853842+ranraok@users.noreply.github.com>
2020-03-04 14:02:36 -0500

    backup commit

--- better-professor2/src/components/Navigation.js
+++ better-professor2/src/components/Navigation.js
@@ -10,10 +10,12 @@ function Navigation() {
     <Nav>
 
         <NavLinksContainer>
-            <Link className="link">Home</Link>
-            <Link className="link">Home</Link>
-            <Link className="link">Home</Link>
-            <Link className="link">Home</Link>
+            <Link className='link'>Dashboard</Link>
+            <Link className="link">Student List List</Link>
+            <Link className="link">Project List</Link>
+            <Link className="link">My Reminders</Link>
+            <Link className="link">Setings</Link>
+            <Link className="link">Help</Link>
        </NavLinksContainer>
     </Nav>
   );
