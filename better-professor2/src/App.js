a2fe7ac101f8a725bfe21ae55aa30d002fec5088
ranraok <46853842+ranraok@users.noreply.github.com>
2020-03-03 19:17:13 -0500

    removed unneccessary links and routes from previous attempt at login and registration page

--- better-professor2/src/App.js
+++ better-professor2/src/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
 import SignUp from './components/Signup';
-// import Login2 from './components/Login2';
 import './App.css';
-// import Registration from './components/Registration';
+
 
 
 function App() {
@@ -22,21 +21,10 @@ function App() {
             <Link to='/signUp'>SignUp</Link>
           </li>
         </ul>
-        {/* <ul>
-          <li>
-            <Link to='/registration '>Registration</Link>
-          </li>
-        </ul>         */}
-        {/* <ul>
-          <li>
-            <Link to='/login2 '>Login2</Link>
-          </li>
-        </ul> */}
+
         <Switch>
           <Route path='/login' component={Login} />
           <Route path='/signup' component={SignUp} /> 
-          {/* <Route path='/registration' component={Registration} /> 
-          <Route path='/login2' component={Login2} /> */}
         </Switch>
       </div>
     </Router>
